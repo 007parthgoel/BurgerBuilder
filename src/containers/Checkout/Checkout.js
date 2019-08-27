c7d693015836a2952ebbe96cfc3b794cf844ded5
parth <parthgoel007@gmail.com>
2019-08-27 12:55:26 +0530

    added routing and fetch-order page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,25 +1,29 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component{
     state={
-        ingredients:{
-            salad:1,
-            cheese:1,
-            meat:1,
-            bacon:1
-        }
+        ingredients:null,
+        price:0
     }
 
-    componentDidMount(){
+    componentWillMount(){
         const query= new URLSearchParams(this.props.location.search);
         const ingredients={};
+        let price=0;
         for(let param of query.entries()){
             // ['salad','1']
-            ingredients[param[0]]=+param[1];
+            if(param[0]==='price'){
+                price=param[1];
+            }else{
+                ingredients[param[0]]=+param[1];
+            }
+            
         }
-        this.setState({ingredients:ingredients});
+        this.setState({ingredients:ingredients, totalPrice: price});
     }
 
     onCheckoutCancelledHandler=()=>{
@@ -37,6 +41,11 @@ class Checkout extends Component{
                    ingredients={this.state.ingredients}
                    onCheckoutCancelled={this.onCheckoutCancelledHandler}
                    onCheckoutContinued={this.onCheckoutContinuedHandler}/>
+                   <Route path={this.props.match.path + '/contact-data'} 
+                          render={(props)=>(<ContactData 
+                                         ingredients={this.state.ingredients}
+                                         price={this.state.totalPrice}
+                                         {...props} />)}/>
             </div>
         )
     }
